bf63a469702b315190401e2ce54168167f669e23
ElmazMed <elmazgour.mohamed@gmail.com>
2024-07-15 20:12:44 +0100

    Goals api is setup

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const errorHandler = require("./middleware/errMiddleware");
-
 const goals = require("./routes/goalsRouter");
+const connectDB = require("./db/connectDB");
 
 const port = process.env.PORT || 5000;
 
@@ -13,6 +13,13 @@ app.use(express.json());
 app.use("/api/v1/goals", goals);
 
 app.use(errorHandler);
-app.listen(port, () => {
-  console.log(`Server is listening to ${port}`);
-});
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is listenig to ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
--- backend/controllers/goalsControllers.js
+++ backend/controllers/goalsControllers.js
@@ -1,7 +1,9 @@
 const asyncHandler = require("express-async-handler");
+const Goal = require("../models/goalsModel");
 
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ msg: "Get goals" });
+  const goal = await Goal.find();
+  res.status(200).json({ goal });
 });
 
 const postGoal = asyncHandler(async (req, res) => {
@@ -9,15 +11,26 @@ const postGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text");
   }
-  res.status(200).json({ msg: "Create goal" });
+  const goal = await Goal.create(req.body);
+  res.status(200).json({ goal });
 });
 
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ msg: `Update goal with the id of ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(404);
+    throw new Error("Goal not found!");
+  }
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json({ updatedGoal });
 });
 
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ msg: `Delete goal with the id of ${req.params.id}` });
+  const goal = await Goal.findByIdAndDelete(req.params.id);
+
+  res.status(200).json({ goal });
 });
 
 module.exports = { getGoals, postGoal, updateGoal, deleteGoal };
